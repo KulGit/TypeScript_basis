26a24952aca5c0304c02229ec61c393a5a6023a7
KulGit <exciland@yandex.ru>
2022-05-21 12:56:13 +0200

    createFiles

---
+++ src/User.ts
@@ -0,0 +1,12 @@
+import faker from 'faker';
+
+class User {
+    name:string;
+    location: {
+        lat:string;
+        lng:string;
+    }
+    constructor () {
+
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log("Hello world");
+// npmjs.com - faker package
